7954c527d9dcb675e1d365a35ab22f890eae2b76
Roman <888frv888@gmail.com>
2024-10-24 14:32:15 +0300

    Испр

--- module_2_4.py
+++ module_2_4.py
@@ -1,15 +1,16 @@
 numbers: list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] #
 primes: list =[]                                                    # создаем списки
 not_primes: list = []                                               #
-for i in range(1,len(numbers)):                                     # перебираем исходный список
+for i in numbers:                                     # перебираем исходный список
+    if i == 1: continue
     is_prime: bool = True                                           # устанавливаем признак простоты
-    for j in range(1,i):                                            # перебираем делители
-        if numbers[i] % numbers[j] == 0:                            # проверка на остаток от деления
+    for j in range(2,i):                                            # перебираем делители
+        if i % j == 0:                            # проверка на остаток от деления
             is_prime = False                                        # при сработке отменяем признак. Составное.
             break                                                   # нет смысла перебирать дальше. За новым делимым.
     if is_prime:                                                    # проверка признака
-        primes.append(numbers[i])                                   # в простые
+        primes.append(i)                                   # в простые
     else:
-        not_primes.append(numbers[i])                               # в составные
+        not_primes.append(i)                               # в составные
 print('Primes ', primes, '\n')                                      #
 print('Not primes ', not_primes)                                    # вывод списков
