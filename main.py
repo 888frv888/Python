200de690b253a2e67850497656e3052f4ac09a51
Roman <888frv888@gmail.com>
2024-10-28 23:37:48 +0300

    Добавлено дополнительное задание второго модуля module_3_3.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-print('Hi, PyCharm')
-x = 43
-y = 32
-print(x * y)
-print("End line")
+# print('Hi, PyCharm')
+# x = 43
+# y = 32
+# print(x * y)
+# print("End line")
+def my_sqr(n, *args, txt='Сумма квадратов'):
+	s: int = 0
+	for i in args:
+		s += i ** n
+	print(txt + ':', s)
+
+my_sqr(1, 1,2,3,4,5,6, txt='Проба')
