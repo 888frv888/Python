080d08ebc7b1a1986744944ff45a07fb7821b374
Roman <888frv888@gmail.com>
2024-10-29 23:21:34 +0300

    Внесены рекомендации от преподавателя

--- module_3_4.py
+++ module_3_4.py
@@ -7,11 +7,13 @@ def single_root_words(root_word: str, *other_words: str) -> list:			# немно
 	:return: Список однокоренных слов
 	"""
 	same_words: list = []													# пустой список для ответа
-	for i in other_words:													# перебор списка поэлементно
-		if root_word.lower() in i.lower():									# проверка с использованием in
-			same_words.append(i)
-		elif root_word.lower().count(i.lower()):							# проверка с использованием count
-			same_words.append(i)
+	for word in other_words:													# перебор списка поэлементно
+		# if root_word.lower() in word.lower():									# проверка с использованием in
+		# 	same_words.append(word)
+		# elif root_word.lower().count(word.lower()):							# проверка с использованием count
+		# 	same_words.append(word)
+		if root_word.lower() in word.lower() or word.lower() in root_word.lower():	#вариант от преподавателя
+			same_words.append(word)
 	return same_words														# возврат ответа
 result1: list = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2: list = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
