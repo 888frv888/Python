67ccd4332eb0666f8c09b6249604b2680d5ad833
Roman <888frv888@gmail.com>
2024-11-21 23:10:49 +0300

    Модуль 5_4 исправлен и дополнен.

--- Модуль 5/module_5_4.py
+++ Модуль 5/module_5_4.py
@@ -1,23 +1,28 @@
 class House:
 	"""Класс, описывающий некоторые здания"""
 
-	houses_history: list[str] = []
+	houses_history: set = set()
 
 	def __new__(cls, *args, **kwargs):
-		cls.houses_history += [args[0]]
-		return object.__new__(cls)
+		"""Создаем новый объект класса"""
+		cls.houses_history.add(args[0])
+		return super().__new__(cls)
 
 	def __init__(self, name: str, number_of_floors: int):
+		"""Инициализируем созданный объект"""
 		self.name: str = name
 		self.number_of_floors: int = number_of_floors
 
-	def __len__(self):
+	def __len__(self) -> int:
+		"""Возвращаем этажность здания"""
 		return self.number_of_floors
 
-	def __str__(self):
+	def __str__(self) -> str:
+		"""Возвращаем информацию по зданию"""
 		return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
 	def go_to(self, new_floor: int) -> None:
+		"""Метод 'шагает' по этажам снизу вверх"""
 		if not 1 <= new_floor <= self.number_of_floors:
 			print("Такого этажа не существует")
 		else:
@@ -25,9 +30,11 @@ class House:
 				print(floor)
 
 	def __del__(self):
+		"""Выводит сообщение при ликвидации объекта"""
 		print(f"{self.name} снесён, но он останется в истории")
 
-	def __eq__(self, other: object | int):  # Наш экземпляр равен другому объекту?
+	def __eq__(self, other: object | int) -> bool:  # Наш экземпляр равен другому объекту?
+		"""Сравнение объекта"""
 		if isinstance(other, House):
 			# Сравнение с другим экземпляром House:
 			return self.number_of_floors == other.number_of_floors
@@ -38,7 +45,8 @@ class House:
 			# Если сравниваем с чем-то другим, то ошибка типа
 			raise TypeError("Должно быть целое число для сравнения. Или объект класса House")
 
-	def __lt__(self, other: object | int):
+	def __lt__(self, other: object | int) -> bool:
+		"""Сравнение объекта"""
 		if isinstance(other, House):
 			return self.number_of_floors < other.number_of_floors
 		elif isinstance(other, int):
@@ -46,57 +54,62 @@ class House:
 		else:
 			raise TypeError("Должно быть целое число для сравнения. Или объект класса House")
 
-	def __le__(self, other: object | int):
+	def __le__(self, other: object | int) -> bool:
+		"""Сравнение объекта"""
 		if isinstance(other, House):
 			return self.__lt__(other.number_of_floors) or self.__eq__(other.number_of_floors)
 
-	def __add__(self, other: object | int):
+	def __add__(self, other: object | int) -> object:
+		"""Метод складывет этажи здания с другим зданием) или с целым числом"""
 		if isinstance(other, House):
-			self.number_of_floors += other.number_of_floors
-			return self
+			# self.number_of_floors += other.number_of_floors
+			return self.__class__(self.name, self.number_of_floors + other.number_of_floors)
 		elif isinstance(other, int):
-			self.number_of_floors += other
-			return self
+			# self.number_of_floors += other
+			return self.__class__(self.name, self.number_of_floors + other)
 		else:
 			raise TypeError("Должно быть целое число для сложения. Или объект класса House")
 
-	def __iadd__(self, other: object | int):
+	def __iadd__(self, other: object | int) -> object:
+		"""То же сложение"""
 		self.__add__(other)
 		return self
 
-	def __radd__(self, other: object | int):
+	def __radd__(self, other: object | int) -> object:
+		"""То же сложение"""
 		self.__add__(other)
 		return self
 
-	def __sub__(self, other: object | int):
+	def __sub__(self, other: object | int) -> object:
+		"""Вычитание этажей объектов"""
 		if isinstance(other, House):
-			self.number_of_floors -= other.number_of_floors
-			return self
+			# self.number_of_floors -= other.number_of_floors
+			return self.__class__(self.name, self.number_of_floors - other.number_of_floors)
 		elif isinstance(other, int):
-			self.number_of_floors -= other
-			return self
+			# self.number_of_floors -= other
+			return self.__class__(self.name, self.number_of_floors - other)
 		else:
 			raise TypeError("Должно быть целое число для вычитания. Или объект класса House")
 
 
 if __name__ == '__main__':
-	# h1 = House('ЖК Эльбрус', 10)
-	# h2 = House('ЖК Акация', 20)
-	# print(h1)
-	# print(h2)
-	# print(h1 == h2)  # __eq__
-	# h1 = h1 + 10  # __add__
-	# print(h1)
-	# print(h1 == h2)
-	# h1 += 10  # __iadd__
-	# print(h1)
-	# h2 = 10 + h2  # __radd__
-	# print(h2)
-	# print(h1 > h2)  # __gt__
-	# print(h1 >= h2)  # __ge__
-	# print(h1 < h2)  # __lt__
-	# print(h1 <= h2)  # __le__
-	# print(h1 != h2)  # __ne__
+	h1 = House('ЖК Эльбрус', 10)
+	h2 = House('ЖК Акация', 20)
+	print(h1)
+	print(h2)
+	print(h1 == h2)  # __eq__
+	h1 = h1 + 10  # __add__
+	print(h1)
+	print(h1 == h2)
+	h1 += 10  # __iadd__
+	print(h1)
+	h2 = 10 + h2  # __radd__
+	print(h2)
+	print(h1 > h2)  # __gt__
+	print(h1 >= h2)  # __ge__
+	print(h1 < h2)  # __lt__
+	print(h1 <= h2)  # __le__
+	print(h1 != h2)  # __ne__
 	h1 = House('ЖК Эльбрус', 10)
 	print(House.houses_history)
 	h2 = House('ЖК Акация', 20)
