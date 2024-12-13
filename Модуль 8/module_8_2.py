350d6c4514caa5080734bee4ac442967a281a9ef
Roman <888frv888@gmail.com>
2024-12-14 00:58:42 +0300

    Исправлен module_8_2.py

--- Модуль 8/module_8_2.py
+++ Модуль 8/module_8_2.py
@@ -1,29 +1,27 @@
 def personal_sum(numbers):
-	result = 0
-	incorrect_data = 0
-	for element in numbers:
-		try:
-			result += element
-		except TypeError:
-			incorrect_data += 1
-			print(f"Некорректный тип данных для подсчета суммы {element}")
-			continue
-	return result, incorrect_data
+    result = 0
+    incorrect_data = 0
+    for element in numbers:
+        try:
+            result += element
+        except TypeError:
+            incorrect_data += 1
+            print(f"Некорректный тип данных для подсчета суммы {element}")
+            continue
+    return result, incorrect_data
+
 
 def calculate_average(numbers):
-	try:
-		average = personal_sum(numbers)
-		summa = average[0]
-		dlinna = len(numbers) - average[1]
-		average = summa / dlinna
-	except ZeroDivisionError:
-		return 0
-	except TypeError:
-		print("В numbers записан некорректный тип данных")
-		return None
-	return average
+    try:
+        average = personal_sum(numbers)
+        return average[0] / (len(numbers) - average[1])
+    except ZeroDivisionError:
+        return 0
+    except TypeError:
+        print("В numbers записан некорректный тип данных")
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
-print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')  # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')  # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}')  # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')  # Всё должно работать
