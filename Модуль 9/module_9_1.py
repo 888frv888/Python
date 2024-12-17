80754bf9705e7f8d7e9630da5a2faccc33f1b135
Roman <888frv888@gmail.com>
2024-12-17 22:42:16 +0300

    Добавлен 9-й модуль. Добавлен module_9_1.py

---
+++ Модуль 9/module_9_1.py
@@ -0,0 +1,11 @@
+def apply_all_func(int_list: list[int or float], *functions) -> dict:
+	"""Функция обрабатывает список чисел различными функциями"""
+	result = dict()
+	for func in functions:
+		result[func.__name__] = func(int_list)
+	return result
+
+
+if __name__ == '__main__':
+	print(apply_all_func([6, 20, 15, 9], max, min))
+	print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
