04a55a831abda7d00df2b7f42f817e0ce59e751d
Roman <888frv888@gmail.com>
2025-01-06 09:57:35 +0300

    module_9_6.pu update

--- Модуль 9/module_9_6.py
+++ Модуль 9/module_9_6.py
@@ -1,10 +1,12 @@
-def all_variants(text):
-	for i in range(1, len(text)+1):
-		fin=len(text)-i+1
-		for j in range(fin):
-			string = text[j:j+i]
-			yield string
+def all_variants(text: str)->str:
+    for i in range(1, len(text) + 1):
+        fin = len(text) - i + 1
+        for j in range(fin):
+            string = text[j:j + i]
+            yield string
+
 
 if __name__ == "__main__":
- for i in all_variants("abc"):
-	 print(i)
+    a = all_variants
+    for i in a("abc"):
+        print(i)
